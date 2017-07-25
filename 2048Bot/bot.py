0da8b68238aa2721476aa12f56495be62bffb2bd
ComedicChimera <forlornsisu@gmail.com>
2017-07-25 15:00:21 -0400

    added ze things ron work now

--- 2048Bot/bot.py
+++ 2048Bot/bot.py
@@ -3,41 +3,45 @@ from random import *
 from numpy import *
 
 moves = [ "'w'", "'s'", "'a'", "'d'"]
+recordedMoves = []
 def GetBotMove(grid):
-    print(grid)
-    tiles = []
-    for i in range(4):
-        for j in range(4):
-           tiles.append(grid[i][j])
-    x = 2
-    for _ in range(11):
-        if(x in tiles):
-            tiles.remove(x)
-        x *= 2
-    indexes = zeros(4, 4)
+    maxlist = []
+    for item in range(3):
+        maxlist.append(max(grid[item]))
+    maximum = max(maxlist)
     while True:
-        maxVal = max(tiles)
-        indicesList = []
-        for num in range(3):
-            if(maxVal in grid[num]):
-                indices = [i for i, x in enumerate(grid[num]) if x == maxVal]
-                for item in indices:
-                    indicesList.append((num, item))
-        if(len(indicesList) > 2):
-            for item in indicesList:
-                indexes[item[0]][item[1]] = 1
-        else:
-            indexes[indicesList[0][0]][indicesList[0][1]] = 1
-            indexes[indicesList[1][0]][indicesList[1][1]] = 1
-        #temporary
+        maxVals = []
+        x = 0
+        for i in grid:
+            if(maximum in i):
+                val = argwhere(maximum)
+                maxVals.append((x, val))
+            x += 1
+        for j in range(len(maxVals) - 1):
+            if(maxVals[j][0] == maxVals[j + 1][0]):
+                for item in recordedMoves:
+                    if(item[0] == ((maxVals[j])(maxVals[j + 1]))):
+                        return item[1]
+                    else:
+                        randMove = GetRandMove()
+                        recordedMoves.append((((maxVals[j])(maxVals[j + 1])), randMove, 0))
+                        return randMove
+            elif(maxVals[j][1] == maxVals[j + 1][1]):
+                for item in recordedMoves:
+                    if(item[0] == ((maxVals[j])(maxVals[j + 1]))):
+                        return item[1]
+                    else:
+                        randMove = GetRandMove()
+                        recordedMoves.append((((maxVals[j])(maxVals[j + 1])), randMove, 0))
+                        return randMove
+            else:
+                return GetRandMove()
         break
-    if(len(indexes) > 0):
-        print("xd")
-    else:
-        sleep(0.1)
-        global moves
-        rand = randint(0, 3)
-        return moves[rand]
 
+def GetRandMove():
+    sleep(0.1)
+    global moves
+    rand = randint(0, 3)
+    return moves[rand]
 
 
